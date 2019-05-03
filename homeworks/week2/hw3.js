ccbd8257a2beecf9668f6e9c6ea85384a3cebfc1
Eife <husky76101@gmail.com>
2019-05-03 14:25:31 +0800

    week2hw

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  return str;
+  // 字串反轉
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str[i];
+  }
+  return newStr;
 }
 
-reverse('hello');
+console.log(reverse('1,2,3,2,1'));
