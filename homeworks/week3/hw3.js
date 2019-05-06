8c00457277618012bf555e104040593c2b1b5051
Eife <husky76101@gmail.com>
2019-05-06 19:39:34 +0800

    week3hw

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,12 @@
 function isPrime(n) {
-  console.log(n);
+  // 判斷質數
+  /* 判斷是否有除了自己及 1 以外的其他因數，這邊須注意 1 不是質數，所以 1 拿出來另外判斷 */
+  if (n > 1 && n <= 100000) {
+    for (let i = 2; i < n; i += 1) {
+      if (n % i === 0) return false;
+    }
+  } else if (n === 1) return false;
+  return true;
 }
 
 module.exports = isPrime;
